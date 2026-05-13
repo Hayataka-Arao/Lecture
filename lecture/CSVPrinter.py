ce10da1ad606c16366601ae3b04f7c8cce42dd3f
arao <ppen6726@gmail.com>
2026-05-13 11:39:18 +0900

    Fix SonarCloud issue: Use list() constructor

--- lecture/CSVPrinter.py
+++ lecture/CSVPrinter.py
@@ -7,5 +7,5 @@ class CSVPrinter:
     def read(self):
         with open(self.filename) as f:
             reader = csv.reader(f)
-            lines = [row for row in reader]
+            lines = list(reader)
         return lines
